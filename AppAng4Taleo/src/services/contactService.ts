59b7fe5977baafcd43048e19c376b65940b29114
kodja <kodja@192.168.0.28>
2018-02-26 14:25:58 +0100

    Add firstName

--- AppAng4Taleo/src/services/contactService.ts
+++ AppAng4Taleo/src/services/contactService.ts
@@ -3,7 +3,8 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class ContactService{
   info= {
-    name: "Kodjane",
+    lastName: "Kodjane",
+    firstName: "Aimé Dieudonné",
     email: "kodjane09@hotmail.com",
     tel: '0032471352401',
     company: "Taleo Consulting"
